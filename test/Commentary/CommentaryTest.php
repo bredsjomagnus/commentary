91dca0341e8d4283815e3518153b8d1036dbcd6e
Magnus Andersson <bredsjomagnus@yahoo.se>
2017-10-12 10:44:09 +0100

    Readme and add test

--- test/Commentary/CommentaryTest.php
+++ test/Commentary/CommentaryTest.php
@@ -9,36 +9,36 @@ class CommentaryTest extends \PHPUnit_Framework_TestCase
     /**
      * Test case Commentary
      */
-    // public function testCommInsert()
-    // {
-    //     // DI
-    //     $di  = new \Anax\DI\DIFactoryConfig("di.php");
-    //
-    //     // Commentary
-    //     $comm = new \Maaa16\Commentary\Commentary();
-    //     $comm->setDI($di);
-    //
-    //     // Get comments before change
-    //     $resbefore = $comm->getComment();
-    //     $numberbefore = count($resbefore);
-    //
-    //     // insert one comment
-    //     $commentOn = 1;
-    //     $username = 'user';
-    //     $email = 'user@email.com';
-    //     $comment = 'En kommentar';
-    //     $comm->addComment($commentOn, $username, $email, $comment);
-    //
-    //     // Get comments before change
-    //     $resafter = $comm->getComment();
-    //     $numberafter = count($resafter);
-    //
-    //     // Differense numberafter - numberbefore
-    //     $diffnumber = intval($numberafter) - intval($numberbefore);
-    //
-    //     // Kollar att differensen mellan antalet är 1
-    //     $this->assertEquals($diffnumber, 1);
-    // }
+    public function testCommInsert()
+    {
+        // DI
+        $di  = new \Anax\DI\DIFactoryConfig("di.php");
+
+        // Commentary
+        $comm = new \Maaa16\Commentary\Commentary();
+        $comm->setDI($di);
+
+        // Get comments before change
+        $resbefore = $comm->getComment();
+        $numberbefore = count($resbefore);
+
+        // insert one comment
+        $commentOn = 1;
+        $username = 'user';
+        $email = 'user@email.com';
+        $comment = 'En kommentar';
+        $comm->addComment($commentOn, $username, $email, $comment);
+
+        // Get comments before change
+        $resafter = $comm->getComment();
+        $numberafter = count($resafter);
+
+        // Differense numberafter - numberbefore
+        $diffnumber = intval($numberafter) - intval($numberbefore);
+
+        // Kollar att differensen mellan antalet är 1
+        $this->assertEquals($diffnumber, 1);
+    }
 
     public function testSlugify()
     {
