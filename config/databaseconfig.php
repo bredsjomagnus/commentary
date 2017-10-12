37b381f208bb2460ec2118079725d801b34df55e
Magnus Andersson <bredsjomagnus@yahoo.se>
2017-10-12 08:17:09 +0100

    config database

--- config/databaseconfig.php
+++ config/databaseconfig.php
@@ -13,20 +13,20 @@
  *
  */
 
-$dbconfig = [
-        "dsn"             => "mysql:host=host;dbname=databasename;",
-        "username"        => "username",
-        "password"        => "password",
-        "driver_options"  => [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
-        "fetch_mode"      => \PDO::FETCH_OBJ,
-        "table_prefix"    => null,
-        "session_key"     => "Anax\Database",
-
-        // True to be very verbose during development
-        "verbose"         => true,
-
-        // True to be verbose on connection failed
-        "debug_connect"   => true,
-    ];
-
-return $dbconfig;
+// $dbconfig = [
+//         "dsn"             => "mysql:host=host;dbname=databasename;",
+//         "username"        => "username",
+//         "password"        => "password",
+//         "driver_options"  => [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
+//         "fetch_mode"      => \PDO::FETCH_OBJ,
+//         "table_prefix"    => null,
+//         "session_key"     => "Anax\Database",
+//
+//         // True to be very verbose during development
+//         "verbose"         => true,
+//
+//         // True to be verbose on connection failed
+//         "debug_connect"   => true,
+//     ];
+//
+// return $dbconfig;
