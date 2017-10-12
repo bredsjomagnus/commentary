a817197586077bfba0ea3808cc28d0b0156354e5
Magnus Andersson <bredsjomagnus@yahoo.se>
2017-10-12 07:43:31 +0100

    test config

--- test/config.php
+++ test/config.php
@@ -1,15 +1,16 @@
 <?php
-
-/**
- * Sample configuration file for test configuration.
- */
-
-
 /**
  * Define essential Anax paths, end with /
  */
 define("ANAX_INSTALL_PATH", realpath(__DIR__ . "/.."));
 define("ANAX_APP_PATH", ANAX_INSTALL_PATH);
+
+/**
+ * Sample configuration file for test configuration.
+ */
+require ANAX_INSTALL_PATH . "/config/anaxdatabase.php";
+
+
 
 
 
