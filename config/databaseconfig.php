0be906f059f0dde4352fd37b7b1a46850938f087
Magnus Andersson <bredsjomagnus@yahoo.se>
2017-10-12 08:22:17 +0100

    config database

--- config/databaseconfig.php
+++ config/databaseconfig.php
@@ -13,20 +13,20 @@
  *
  */
 
-$dbconfig = [
-        // "dsn"             => "mysql:host=host;dbname=databasename;",
-        // "username"        => "username",
-        // "password"        => "password",
-        // "driver_options"  => [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
-        // "fetch_mode"      => \PDO::FETCH_OBJ,
-        // "table_prefix"    => null,
-        // "session_key"     => "Anax\Database",
-        //
-        // // True to be very verbose during development
-        // "verbose"         => true,
-        //
-        // // True to be verbose on connection failed
-        // "debug_connect"   => true,
-    ];
+ $dbconfig = [
+         "dsn"             => "mysql:host=blu-ray.student.bth.se;dbname=maaa16;",
+         "username"        => "maaa16",
+         "password"        => "tWabjxC6eEH6",
+         "driver_options"  => [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
+         "fetch_mode"      => \PDO::FETCH_OBJ,
+         "table_prefix"    => null,
+         "session_key"     => "Anax\Database",
+
+         // True to be very verbose during development
+         "verbose"         => false,
+
+         // True to be verbose on connection failed
+         "debug_connect"   => false,
+     ];
 
 return $dbconfig;
